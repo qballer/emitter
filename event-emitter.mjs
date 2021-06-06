fc3435e6a303dbc221f04e229704dabae021b145
Doron Tsur <qballer@gmail.com>
2021-06-06 16:45:19 +0300

    save point

--- event-emitter.mjs
+++ event-emitter.mjs
@@ -1,26 +1,2 @@
 export class MyEventEmitter { 
-    listeners = {}
-    on(name, callback){
-        if (typeof name !== 'string') {
-            throw new Error('event name is not string')
-        }
-        if (typeof callback !== 'function') {
-            throw new Error('callback is not a function') 
-        }
-        if (!this.listeners[name]) {
-            this.listeners[name] = []
-        }
-        this.listeners[name].push(callback)
-    }
-
-    emit(name, ...args) {
-        if (typeof name !== 'string') {
-            throw new Error('event name is not string')
-        }
-        const listeners = this.listeners[name] || []
-        listeners.forEach(listener => {
-            listener(...args)
-            // setTimeout(() => listener(...args), 0)
-        })
-    }
 }
